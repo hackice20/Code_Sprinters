03eaa646d1bf736c79cff2e951e1d051ba368e2d
Avishkar Dhanorkar <avidhanorkar23@gmail.com>
2025-02-22 16:28:54 +0530

    initialize frontend with React, Vite, and essential components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,70 @@
+import React from "react"; 
+import Navbar from "./components/Common/Navbar";
+import Footer from "./components/Common/Footer";
+import LandingPage from "./Pages/LandingPage";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Auth from "./Pages/Auth";
+import Certificate from "./Pages/Certificate";
+import Dashboard from "./Pages/Dashboard";
+import { AuthProvider } from "./context/authContext";
+import ExploreCourses from "./Pages/ExploreCourses";
+import CoursePage from "./Pages/Course";
+import AdminDashboard from "./Pages/Admin/Dashboard";
+import CreateCourse from "./Pages/Admin/CreateCourse";
+import CourseLearningPage from "./Pages/Course/learn";
+import AdminAuth from "./Pages/Admin/Auth";
+import CreateQuiz from "./Pages/Admin/CreateQuiz";
+import ProtectedRoute from "./components/Common/ProtectedRoutes";
+import QuizApp from "./Pages/Quiz/TakeQuiz";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="">
+        <AuthProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<LandingPage />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/certificate" element={<Certificate />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/exploreCourses" element={<ExploreCourses />} />
+            <Route path="/course/:id" element={<CoursePage />} />
+            <Route path="/learn/:id" element={<CourseLearningPage />} />
+            <Route path="/admin/auth" element={<AdminAuth />} />
+            <Route path="/takeQuiz/:id" element={<QuizApp />} />
+            {/* Protected Admin Routes */}
+            <Route 
+              path="/admin/dashboard" 
+              element={
+                <ProtectedRoute>
+                  <AdminDashboard />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/admin/createCourse" 
+              element={
+                <ProtectedRoute>
+                  <CreateCourse />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/admin/:id/createQuiz" 
+              element={
+                <ProtectedRoute>
+                  <CreateQuiz />
+                </ProtectedRoute>
+              } 
+            />
+          </Routes>
+          <Footer />
+        </AuthProvider>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/auth/register.jsx
@@ -0,0 +1,133 @@
+import { GraduationCap } from "lucide-react";
+import React, { useState } from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const Register = ({ setTab }) => {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`http://localhost:3000/api/auth/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: user.username,
+          email: user.email,
+          password: user.password,
+        }),
+        mode: "cors",
+      });
+
+      if (!response.ok) throw new Error("Failed to register");
+
+      const data = await response.json();
+      console.log(data);
+      setTab("login");
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex flex-col items-center justify-center">
+        <GraduationCap className="w-16 h-16" />
+        <h1 className="text-3xl font-bold">Welcome Back</h1>
+        <p className="text-center text-gray-600">
+          Join Now – Empower Your Learning Journey!
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <form className="flex flex-col gap-3 mt-5" onSubmit={handleSubmit}>
+          {/* Username Input Field */}
+          <div className="flex flex-col">
+            <label className="font-semibold text-[16px] text-left">
+              Username
+            </label>
+            <Input
+              type="text"
+              className="w-full"
+              placeholder="Enter your username"
+              name="username"
+              value={user.username}
+              onChange={handleChange}
+            />
+          </div>
+
+          {/* Email Input Field */}
+          <div className="flex flex-col">
+            <label className="font-semibold text-[16px] text-left">
+              Email Address
+            </label>
+            <Input
+              type="email"
+              className="w-full"
+              placeholder="you@example.com"
+              name="email"
+              value={user.email}
+              onChange={handleChange}
+            />
+          </div>
+
+          {/* Password Input Field */}
+          <div className="flex flex-col">
+            <label className="font-semibold text-[16px] text-left">
+              Password
+            </label>
+            <Input
+              type="password"
+              className="w-full"
+              placeholder="••••••••"
+              name="password"
+              value={user.password}
+              onChange={handleChange}
+            />
+          </div>
+
+          {/* Submit Button */}
+          <div className="w-full mt-5">
+            <Button
+              className="w-full text-sm tracking-wider uppercase px-12 py-6 bg-primary hover:bg-purple-700 bg-purple-600"
+              type="submit"
+            >
+              Sign Up
+            </Button>
+          </div>
+        </form>
+
+        {/* Login Link */}
+        <div className="mt-1">
+          <p className="text-center">
+            Already have an account?{" "}
+            <span
+              className="text-purple-600 font-semibold cursor-pointer"
+              onClick={() => {
+                setTab("login");
+              }}
+            >
+              Login Now
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
